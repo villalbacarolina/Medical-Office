38d04cd6e54fd4a0287e49e68b868d2d2d50642d
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2024-11-11 13:36:01 -0300

    feat: service class PatientService

--- patients/src/main/java/com/todocodeacademy/patients/service/PatientService.java
+++ patients/src/main/java/com/todocodeacademy/patients/service/PatientService.java
@@ -1,5 +1,40 @@
 package com.todocodeacademy.patients.service;
 
-public class PatientService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.todocodeacademy.patients.model.Patient;
+import com.todocodeacademy.patients.repository.IPatientRepository;
+
+public class PatientService implements ICrudService<Patient>{
+
+	@Autowired
+	private IPatientRepository patientRepository;
+	
+	@Override
+	public List<Patient> get() {
+		return patientRepository.findAll();
+	}
+
+	@Override
+	public void save(Patient pat) {
+		patientRepository.save(pat);
+	}
+
+	@Override
+	public void delete(Long patientId) {
+		patientRepository.deleteById(patientId);
+	}
+
+	@Override
+	public Patient find(Long patientId) {
+		return patientRepository.findById(patientId).orElse(null);
+	}
+
+	@Override
+	public void edit(Patient pat) {
+		this.save(pat);
+	}
 
 }
